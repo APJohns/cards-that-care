19f9d79cfb64da05c7dd22dc38a99c25a020a10d
APJohns <ashleypjohns@gmail.com>
2020-09-30 01:17:16 -0400

    Add pagination

--- src/components/modules/store/Catalogue.js
+++ src/components/modules/store/Catalogue.js
@@ -11,8 +11,53 @@ class Catalogue extends React.Component {
     history: PropTypes.object
   };
 
+  state = {
+    cardsPage: [],
+    currentPage: 1,
+    pageSize: 12,
+    startIndex: 0,
+    endIndex: 12,
+    isPrevDisabled: true,
+    numPages: 1
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      this.setState({
+        cardsPage: Object.keys(this.props.cards).slice(this.state.startIndex, this.state.endIndex),
+        numPages: Math.ceil(Object.keys(this.props.cards).length / this.state.pageSize)
+      });
+    }
+  }
+
   isSelected(key) {
     return this.props.selectedCards && this.props.selectedCards[key] ? true : false;
+  }
+
+  nextPage = () => {
+    if (this.state.startIndex + this.state.pageSize <= Object.keys(this.props.cards).length) {
+      const startIndex = this.state.startIndex + this.state.pageSize;
+      const endIndex = this.state.endIndex + this.state.pageSize;
+      this.setState({
+        currentPage: this.state.currentPage + 1,
+        startIndex,
+        endIndex,
+        cardsPage: Object.keys(this.props.cards).slice(startIndex, endIndex)
+      });
+    }
+  }
+
+  prevPage = () => {
+    if (this.state.startIndex - this.state.pageSize >= 0) {
+      const startIndex = this.state.startIndex - this.state.pageSize;
+      const endIndex = this.state.endIndex - this.state.pageSize;
+      this.setState({
+        currentPage: this.state.currentPage - 1,
+        startIndex,
+        endIndex,
+        cardsPage: Object.keys(this.props.cards).slice(startIndex, endIndex)
+      });
+    }
   }
 
   render() {
@@ -21,7 +66,7 @@ class Catalogue extends React.Component {
         <h1 className="mt-3">Catalogue</h1>
         <p>All profits made before October 1st go to the Pan-Mass Challenge. After October 1st, profits will be donated directly to Dana-Farber.</p>
         <div className="row">
-          {Object.keys(this.props.cards).map((key, i) => (
+          {this.state.cardsPage.map((key, i) => (
             <Card
               key={key}
               addToCart={this.props.addToCart}
@@ -30,6 +75,26 @@ class Catalogue extends React.Component {
               isSelected={this.isSelected(key)}
               {...this.props.cards[key]} />
           ))}
+        </div>
+        <div className="row align-items-center">
+          <p className="col-auto ml-auto">Page {this.state.currentPage} of {this.state.numPages}</p>
+          <nav className="col-auto" aria-label="Card pagination">
+            <ul className="pagination">
+              <li className={'page-item' + (this.state.currentPage === 1 && ' disabled')}>
+                <button onClick={this.prevPage} className="page-link" aria-disabled={this.state.currentPage === 1}>Previous</button>
+              </li>
+              {/* TODO: dynamically create these */}
+              {/* <li className="page-item"><button className="page-link">{this.state.currentPage - 1}</button></li>
+              <li className="page-item active" aria-current="page">
+                <button className="page-link" href="#">{this.state.currentPage} <span className="sr-only">(current)</span></button>
+              </li>
+              <li className="page-item"><button className="page-link">{this.state.currentPage + 1}</button></li>
+              <li className="page-item"><button className="page-link">{this.state.currentPage + 2}</button></li> */}
+              <li className={'page-item' + (this.state.currentPage === this.state.numPages && ' disabled')}>
+                <button onClick={this.nextPage} className="page-link" aria-disabled={this.state.currentPage === this.state.numPages}>Next</button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </section>
     );
